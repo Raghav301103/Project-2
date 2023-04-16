0b3e32f6a863e8a3bb6936fefb7e4b7a705ccf8f
Raghav Sharma <smarty@Raghavs-MacBook-Air.local>
2023-04-16 22:33:48 +0530

    Fourth commit

--- drug.js
+++ drug.js
@@ -1,30 +1,44 @@
 // Get a reference to the DOM element where the results will be displayed
 const drugInfoList = document.getElementById("drug-info");
+const Raghav=document.getElementById("Eklavya");
+fetch('https://api.fda.gov/drug/drugsfda.json?limit=100')
+  .then(response => response.json())
+  .then(data => {
+    const drugs = data.results;
+    // parse the JSON response into a JavaScript object
 
-// Define the API endpoint URL and query parameters
-const apiUrl = "https://api.fda.gov/drug/drugsfda.json";
-const apiParams = new URLSearchParams({
-  count: "products.route.exact",
-  limit: 10
-});
-
+    const genericNames = drugs.map(drug => drug.products[0].brand_name);
+    displayResults(genericNames); // Call the displayResults function with the array of generic names
+    console.log(genericNames);
+  })
 // Define a function to display the results
 function displayResults(results) {
   // Loop through the results and add each one to the list
   results.forEach(result => {
     const listItem = document.createElement("li");
-    listItem.textContent = `${result.term}: ${result.count}`;
+    listItem.textContent = result;
+    const Garima = document.createElement("button");
+    Garima.innerText = "Click for detail";
+
+    drugInfoList.appendChild(Garima);
     drugInfoList.appendChild(listItem);
+    
+    Garima.addEventListener("click", function (event) {
+      event.preventDefault();
+      displayDrugInfo(result);
+      console.log("Garima is love")
+    });
   });
 }
-
-// Make the API call and handle the response
-fetch(`${apiUrl}?${apiParams}`)
-  .then(response => response.json())
-  .then(data => {
-    const results = data.results;
-    displayResults(results);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+function displayDrugInfo(genericName) {
+  fetch(`https://api.fda.gov/drug/drugsfda.json?limit=1`)
+    .then(response => response.json())
+    .then(data => {
+      const drug = data.results[0];
+      // display information about the drug in the DOM, e.g.:
+      const drugInfo = document.getElementById("Eklavya");
+      drugInfo.innerText="productnumber="+data.application_number+ "  dosageform="+data.sponsor_name
+       
+      drugInfo.style.display="block";
+    })
+}
